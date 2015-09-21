805b30bdc9cc835adc529e66231c457b583dae28
Maksim Pecherskiy <max@maksimize.com>
2015-09-20 18:53:09 -0700

    Filtering on water / sewer consumption ranges.

--- server.R
+++ server.R
@@ -6,16 +6,23 @@ library(shiny)
 
 shinyServer(function(input, output) {
 
-  output$waterUsage <- renderPlot({
-    inputCpcode = input$cpcode
-    plotData <- bc
-    if (inputCpcode != "")
-      plotData <- filter(bc, cpcode == inputCpcode)
-    g <- ggplot(data = plotData, aes(x = "")
 
+  output$waterUsage <- renderPlot({
+    print("Running Cal")
+    wu <- bc %>%
+      filter(water_cons >= input$wc_range[1] & water_cons <= input$wc_range[2]) %>%
+      filter(sewer_cons >= input$sc_range[1] & sewer_cons <= input$sc_range[2]) %>%
+      group_by(cpcode, cpname) %>%
+      summarise(water_cons = sum(water_cons))
+
+    g <- ggplot(data = wu, aes(x = cpname, y = water_cons)) +
+        geom_bar(stat = "identity") +
+        coord_flip()
+    g
 
   })
 
-  output$debug <- renderText(input$cpcode)
+  output$wc_val <- renderPrint(input$wc_range)
+  output$sc_val <- renderPrint(input$sc_range)
 
 })
--- ui.R
+++ ui.R
@@ -11,12 +11,26 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
-      selectInput(inputId = "cpcode",
-                  selected = 15,
-                  multiple = FALSE,
-                  label = h3("Community Planning District"),
-                  choices = getCPDList(TRUE)),
-      textOutput("debug")
+#       selectInput(inputId = "cpcode",
+#                   selected = 15,
+#                   multiple = FALSE,
+#                   label = h3("Community Planning District"),
+#                   choices = getCPDList(TRUE)),
+           sliderInput("wc_range",
+                       label = h3("Water Consumption"),
+                       min = 0,
+                       max = 100,
+                       value = c(0, 100)),
+
+           sliderInput("sc_range",
+                       label = h3("Sewer Consumption"),
+                       min = 0,
+                       max = 100,
+                       value = c(0, 100)),
+        fluidRow(
+            column(4, verbatimTextOutput("wc_val")),
+            column(4, verbatimTextOutput("sc_val"))
+        )
     ),
 
     # Show a plot of the generated distribution
