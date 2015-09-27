b2958f918d86361e87077b51ac56c2ce6f80e6e7
Maksim Pecherskiy <max@maksimize.com>
2015-09-26 18:05:07 -0700

    Remove global.R

--- server.R
+++ server.R
@@ -1,9 +1,35 @@
-source('./global.R')
-
+library(dplyr)
+library(lubridate)
+library(rgdal)
+library(ggplot2)
 library(shiny)
 
-#bc is now available.
 
+#Preprocess the data
+wbill <- read.csv("./data/billing/bill_sample_complete.csv")
+# Load SHP
+com_plan <- readOGR(
+    paste0("./data/shp/community_plan_sd"),
+    layer = "community_plan_sd")
+
+bc <- wbill %>%
+    mutate(billing_frequency = toupper(billing_frequency),
+           loc_in_city = toupper(loc_in_city)) %>%
+    mutate(bill_post_date = mdy(bill_date_posting_date),
+           meter_read_date = mdy(meter_read_date)) %>%
+    # Remove rows that are not bi-monthly since that will skew the data:
+    filter(billing_frequency == 'B') %>%
+    # Remove rows that are not in a city
+    filter(loc_in_city == 'IN') %>%
+    select(
+        id,
+        cpcode,
+        cpname,
+        water_cons = water_consumption,
+        sewer_cons = sewer_consumption
+    )
+
+# Shiny Server!
 shinyServer(function(input, output) {
    output$waterUsage <- renderPlot({
        wu <- reactive({
